443934a3b355663a93c00db4af9739901a687683
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 18:36:42 +0300

    Proje dosyası ekle.

---
+++ DNGS-SAYAC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DNGS_SAYAC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+
+            for (int i = 1000; i >=0; i-=5)
+            {
+                Console.Write("{0,5}", i);
+                sayac++;
+            }
+            Console.WriteLine("\n\n Elaman sayisi:{0}", sayac);
+        }
+    }
+}
